f899e21b35189a0b4336ecd4f13599d50c22adf8
piotrkumala <piotrek.kumala@gmail.com>
2022-01-17 22:54:17 +0100

    add basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ItemsListComponent } from './items-list/items-list.component';
+import { ItemsListModule } from './items-list/items-list.module';
+import { HeaderModule } from './header/header.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+const routes: Routes = [
+  { path: 'list', component: ItemsListComponent },
+  { path: '', redirectTo: '/list', pathMatch: 'full' },
+];
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    RouterModule,
+    HeaderModule,
+    ItemsListModule,
+    RouterModule.forRoot(routes),
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/items-list/items-list.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ItemsListComponent } from './items-list.component';
+
+@NgModule({
+  declarations: [ItemsListComponent],
+  imports: [CommonModule],
+})
+export class ItemsListModule {}
